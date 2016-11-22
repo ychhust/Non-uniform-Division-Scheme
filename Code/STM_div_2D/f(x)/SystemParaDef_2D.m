20107686278dde8f571d33582bae7ea49aebe6e0
Ye chen <1527559175@qq.com>
2016-11-22 20:11:59 +0800

    十字型阵列，已定标

--- Code/STM_div_2D/f(x)/SystemParaDef_2D.m
+++ Code/STM_div_2D/f(x)/SystemParaDef_2D.m
@@ -6,8 +6,8 @@ function [ant_num,min_spacing,T_rec,array_type]=SystemParaDef_2D()
 %   此函数为系统参数定义模块，功能为：
 %   定义系统天线类型、阵元数目、最小天线间距、接收机亮温等信息。
 
-    array_type = 'y_shape'; %阵列排布类型为十字阵。  *AntennaPositionHELP()        
-    ant_num =73;% 阵元数                
+    array_type = 'Cross_shape'; %阵列排布类型为十字阵。  *AntennaPositionHELP()        
+    ant_num =72;% 阵元数                
     min_spacing =0.5;% 最小间距、基线排列方式单位为波长：  
     T_rec = 300;%接收机温度，即TB。单位：K
  
